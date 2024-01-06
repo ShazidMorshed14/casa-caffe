daaeed13010dec4239f23e110183de9fe4ca544c
ShazidMorshed14 <shazidmorshed14@gmail.com>
2024-01-07 04:18:37 +0600

    navbar with menu added

--- js/main.js
+++ js/main.js
@@ -11,9 +11,18 @@ window.onload = function () {
 
   const menu_btn = document.querySelector(".hamburger");
   const mobile_menu = document.querySelector(".mobile-nav");
+  const nav_close_btn = document.querySelector(".nav_close_btn");
+  const go_back_btn = document.querySelector("#contrast_link");
 
   menu_btn.addEventListener("click", function () {
-    menu_btn.classList.toggle("is-active");
+    mobile_menu.classList.toggle("is-active");
+  });
+
+  nav_close_btn.addEventListener("click", function () {
+    mobile_menu.classList.toggle("is-active");
+  });
+
+  go_back_btn.addEventListener("click", function () {
     mobile_menu.classList.toggle("is-active");
   });
 };
